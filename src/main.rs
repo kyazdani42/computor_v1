0985b3328cbb3ec184d089479f1689a89f1dfc54
kyazdani42 <kyazdani@student.42.fr>
2019-02-26 15:59:41 +0100

    fast parse cli

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let no_args = args.len() == 1;
+    let args_not_correct = args.len() != 3 || args[1].trim() != "-c";
+    if no_args {
+        loop {
+            // might wan't to do the same execution than in else, just add an stdin read first
+        }
+    } else if args_not_correct {
+        println!("usage: computor_v1 | computor_v1 -c \"equation\"");
+    } else {
+        let equation = &args[2];
+        println!("{}", equation);
+    }
 }
+
+// 1) lexing / parsing equation
+// 2) simplify
+// 3) compute
+// 4) display
