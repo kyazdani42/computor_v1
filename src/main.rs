f658eeaea32ac622c9ea5ab3e005617509f97c0d
kiyan yazdani <kyazdani@student.42.fr>
2019-02-27 16:07:08 +0100

    add stdin reader, make equation var types strings

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,32 @@
 use std::env;
+use std::io;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let no_args = args.len() == 1;
-    let args_not_correct = args.len() != 3 || args[1].trim() != "-c";
+    let no_args: bool = args.len() == 1;
+    let args_not_correct: bool = args.len() != 3 || args[1].trim() != "-c";
     if no_args {
         loop {
-            // might wan't to do the same execution than in else, just add an stdin read first
+            println!("Enter the equation: ");
+            let mut equation = String::new();
+            io::stdin().read_line(&mut equation).expect("error: reading stdin failed");
+            execute_program(equation);
         }
     } else if args_not_correct {
-        println!("usage: computor_v1 | computor_v1 -c \"equation\"");
+        let usage = "usage: computor_v1 | computor_v1 -c \"equation\"";
+        print_error(&usage);
     } else {
-        let equation = &args[2];
-        println!("{}", equation);
+        let equation: String = args[2].clone();
+        execute_program(equation);
     }
+}
+
+fn print_error(error: &str) {
+    println!("{}", error);
+}
+
+fn execute_program(equation: String) {
+    println!("{}", equation);
 }
 
 // 1) lexing / parsing equation
