17a0e3b6a830c4a25a29f55d03b8f6ff8c8241a5
kiyan yazdani <kyazdani@student.42.fr>
2019-03-02 19:43:16 +0100

    parser finished

--- src/equation.rs
+++ src/equation.rs
@@ -5,41 +5,44 @@ pub struct Equation {
     pub r_op: Vec<Operation>
 }
 
-#[derive(Debug)]
-pub struct Operation {
-    pub negative: bool,
-    pub value: i64,
-    pub pow: i16,
-}
-
 impl Equation {
   pub fn new(l_op: Vec<Operation>, r_op: Vec<Operation>) -> Equation {
     Equation { l_op, r_op }
   }
 }
 
-impl Operation {
-  pub fn new() -> Operation {
-    Operation { negative: false, value: 0, pow: 0 }
+impl fmt::Display for Equation {
+  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    let l_op = &get_str_from_vec(&self.l_op)[2..];
+    let r_op = get_str_from_vec(&self.r_op);
+    write!(f, "{}= {}", l_op, r_op)
   }
 }
 
-impl fmt::Display for Equation {
-  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-    let l_op = get_str_from_vec(&self.l_op);
-    let r_op = get_str_from_vec(&self.r_op);
-    write!(f, "{} = {}", l_op, r_op)
+#[derive(Debug, PartialEq)]
+pub struct Operation {
+    pub negative: bool,
+    pub value: i64,
+    pub pow: i16,
+}
+
+impl Operation {
+  pub fn new(negative: bool, value: i64, pow: i16) -> Operation {
+    Operation { negative, value, pow }
   }
 }
 
 impl fmt::Display for Operation {
   fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    if self.pow == 0 && self.value == 0 {
+      return write!(f, "0");
+    };
     let sign = if self.negative {
       "-"
     } else {
       "+"
     };
-    write!(f, "{}{} * X^{}", sign, self.value, self.pow)
+    write!(f, "{} {} * X^{}", sign, self.value, self.pow)
   }
 }
 
--- src/parse.rs
+++ src/parse.rs
@@ -1,4 +1,5 @@
 use equation::{Equation, Operation};
+use std::str::Bytes;
 
 const ERR_FORMAT: &str = "Wrong Format !";
 
@@ -6,7 +7,12 @@ pub fn parse(s: String) -> Result<Equation, &'static str> {
     let operations = split_equal(&s)?;
 
     let left_op = get_operation_vec(operations[0])?;
-    let right_op = get_operation_vec(operations[1])?;
+    let right_op;
+    if operations[1] == "0" {
+        right_op = get_operation_vec(operations[1])?;
+    } else {
+        right_op = vec![Operation::new(false, 0, 0)];
+    };
 
     Ok(Equation::new(left_op, right_op))
 }
@@ -32,15 +38,103 @@ fn retain_spaces(operation: &str) -> String {
     cleaned_operation
 }
 
-// string in form a*x^pow(+|-)b*x^pow(+|-)c*x^pow
 fn parse_operations(operations: String) -> Option<Vec<Operation>> {
-    println!("{}", operations);
     let mut result: Vec<Operation> = Vec::new();
+    let mut index: usize = 0;
+    loop {
+        let sliceable_operations = String::from(&operations[index..]);
 
-    result.push(Operation::new());
+        let iterator = sliceable_operations.bytes();
+        let operation_result = get_operation_from_iterator(iterator)?;
+        index += operation_result.index;
+
+        let operation = get_operation_from_str(&operation_result.value, operation_result.operator)?;
+        result.push(operation);
+
+        if index >= operations.len() {
+            break;
+        };
+    }
 
     Some(result)
 }
+
+#[derive(Debug, PartialEq)]
+struct OperationIterationResult {
+    index: usize,
+    operator: Sign,
+    value: String,
+}
+
+impl OperationIterationResult {
+    fn new() -> OperationIterationResult {
+        OperationIterationResult {
+            index: 0,
+            value: String::new(),
+            operator: Sign::Pos
+        }
+    }
+}
+
+fn get_operation_from_iterator(iterator: Bytes) -> Option<OperationIterationResult> {
+    let mut return_value = OperationIterationResult::new();
+    for byte in iterator {
+        match byte {
+            b'0'...b'9' | b'x' | b'^' | b'*' => {
+                return_value.value.push(byte as char);
+            },
+            b'-' | b'+' => {
+                if return_value.index > 0 {
+                    break;
+                }
+                if byte == b'-' {
+                    return_value.operator = Sign::Neg;
+                }
+            },
+            _ => return None,
+        };
+        return_value.index += 1;
+    }
+    Some(return_value)
+}
+
+#[derive(Debug, PartialEq)]
+enum Sign {
+    Pos,
+    Neg    
+}
+
+fn get_operation_from_str(operation_as_str: &str, sign: Sign) -> Option<Operation> {
+    let splitted_by_mult: Vec<&str> = operation_as_str.split('*').collect();
+    if splitted_by_mult.len() != 2 {
+        return None;
+    };
+    let wrapped_num = splitted_by_mult.first()?.parse();
+    let value;
+    if wrapped_num.is_err() {
+        return None;
+    }  else {
+        value = wrapped_num.unwrap();
+    };
+    let splitted_by_pow: Vec<&str> = splitted_by_mult.last()?.split('^').collect();
+    if splitted_by_pow.len() != 2 || splitted_by_pow.first()? != &"x" {
+        return None;
+    };
+    let wrapped_pow = splitted_by_pow.last()?.parse::<i16>();
+    let pow;
+    if wrapped_pow.is_err() {
+        return None;
+    } else {
+        pow = wrapped_pow.unwrap();
+    };
+    let is_neg = match sign {
+        Sign::Neg => true,
+        Sign::Pos => false
+    };
+    Some(Operation::new(is_neg, value, pow))
+}
+
+// -------------- TESTS ----------------- //
 
 #[cfg(test)]
 mod tests {
@@ -65,5 +159,59 @@ mod tests {
         let cleaned = retain_spaces("test  te s t t es t ");
         let expected = String::from("testtesttest");
         assert_eq!(cleaned, expected)
+    }
+
+    #[test]
+    fn test_operation_from_iterator() {
+        let test_value = "14*x^1+13*x^2".bytes();
+        let expected = OperationIterationResult {
+            index: 6,
+            operator: Sign::Pos,
+            value: "14*x^1".to_owned()
+        };
+        let result = get_operation_from_iterator(test_value).unwrap();
+        assert_eq!(result, expected)
+    }
+
+    #[test]
+    fn test_operation_from_iterator_minus() {
+        let test_value = "-14*x^1+13*x^2".bytes();
+        let expected = OperationIterationResult {
+            index: 7,
+            operator: Sign::Neg,
+            value: "14*x^1".to_owned()
+        };
+        let result = get_operation_from_iterator(test_value).unwrap();
+        assert_eq!(result, expected)
+    }
+
+    #[test]
+    fn fail_operation_from_iterator() {
+        let test_value = "14*ax^1+1*2".bytes();
+        let result = get_operation_from_iterator(test_value);
+        assert_eq!(result, None)
+    }
+
+    #[test]
+    fn test_get_operation() {
+        let test_value = "14*x^1";
+        let result = get_operation_from_str(test_value, Sign::Pos).unwrap();
+        let expected = Operation::new(false, 14, 1);
+        assert_eq!(result, expected)
+    }
+
+    #[test]
+    fn test_get_operation_neg() {
+        let test_value = "14*x^2";
+        let result = get_operation_from_str(test_value, Sign::Neg).unwrap();
+        let expected = Operation::new(true, 14, 2);
+        assert_eq!(result, expected)
+    }
+
+    #[test]
+    fn test_get_operation_fail() {
+        let test_value = "14*x^";
+        let result = get_operation_from_str(test_value, Sign::Pos);
+        assert!(result.is_none())
     }
 }
